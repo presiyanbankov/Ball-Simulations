839bbb627db2bd59e55c0d6431cb9912ced53a60
SimPresko <preslibankov@gmail.com>
2025-09-05 20:41:14 +0200

    high resolution smooth scale

--- BallGrower/BallGrower.py
+++ BallGrower/BallGrower.py
@@ -1,8 +1,6 @@
 import os
 import pygame
 import numpy as np
-import random
-import sys
 import colorsys
 
 
@@ -15,9 +13,15 @@ class Ball:
 
 
 pygame.init()
+SCALE = 2
 WIDTH = 450
 HEIGHT = 800
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+high_res = pygame.Surface((WIDTH*SCALE, HEIGHT*SCALE))
+ORIGINAL_WIDTH = WIDTH
+ORIGINAL_HEIGHT = HEIGHT
+WIDTH *= SCALE
+HEIGHT *= SCALE
 clock = pygame.time.Clock()
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -28,16 +32,16 @@ GREEN = (0, 255, 0)
 DARK_GREEN = (0, 150, 0)
 BLUE = (0, 0, 255)
 LIGHTBLUE = (0, 150, 255)
-font = pygame.font.Font('freesansbold.ttf', 30)
+font = pygame.font.Font('freesansbold.ttf', 40 * SCALE)
 
 CIRCLE_CENTER = np.array([WIDTH / 2, HEIGHT / 2], dtype=np.float64)
-CIRCLE_RADIUS = 150
+CIRCLE_RADIUS = 150 * SCALE
 
-BALL_RADIUS = 5
-ball_pos = np.array([WIDTH / 2, HEIGHT / 2 - 120], dtype=np.float64)
+BALL_RADIUS = 5 * SCALE
+ball_pos = np.array([WIDTH / 2, HEIGHT / 2 - 120 * SCALE], dtype=np.float64)
 
-GRAVITY = 0.1
-ball_vel = np.array([2, 3], dtype=np.float64)
+GRAVITY = 0.1 * SCALE
+ball_vel = np.array([2 * SCALE, 3 * SCALE], dtype=np.float64)
 balls = [Ball(ball_pos, ball_vel)]
 counter = 0
 
@@ -84,36 +88,40 @@ while running:
                 proj_v_t = (np.dot(ball.v, t) / np.dot(t, t)) * t
                 ball.v = 2 * proj_v_t - ball.v
                 play_next_snippet()
-                BALL_RADIUS+=1
+                BALL_RADIUS+=1 * SCALE
                 counter+=1
 
-    window.fill(BLACK)
-    pygame.draw.circle(window, WHITE, CIRCLE_CENTER, CIRCLE_RADIUS, 4)
+    high_res.fill(BLACK)
+    pygame.draw.circle(high_res, WHITE, CIRCLE_CENTER, CIRCLE_RADIUS, 4 * SCALE)
     for ball in balls:
         ball_color = hsv_to_rgb(ball.hue, 1, 1)
-        pygame.draw.circle(window, ball_color, ball.pos, BALL_RADIUS)
+        pygame.draw.circle(high_res, ball_color, ball.pos, BALL_RADIUS)
     ball_counter = font.render(f"Balls: {counter}", True, WHITE)
-    if BALL_RADIUS <= 10:
+    if BALL_RADIUS <= 10 * SCALE:
         diff_text = font.render(f"IMPOSSIBLE", True, RED)
-    elif BALL_RADIUS < 20:
+    elif BALL_RADIUS < 20 * SCALE:
         diff_text = font.render(f"EXTRA HARD", True, ORANGE)
-    elif BALL_RADIUS < 35:
+    elif BALL_RADIUS < 35 * SCALE:
         diff_text = font.render(f"HARD", True, YELLOW)
-    elif BALL_RADIUS < 50:
+    elif BALL_RADIUS < 50 * SCALE:
         diff_text = font.render(f"MEDIUM", True, GREEN)
-    elif BALL_RADIUS < 70:
+    elif BALL_RADIUS < 70 * SCALE:
         diff_text = font.render(f"EASY", True, DARK_GREEN)
-    elif BALL_RADIUS < 100:
+    elif BALL_RADIUS < 100 * SCALE:
         diff_text = font.render(f"SUPER EASY", True, BLUE)
-    elif BALL_RADIUS < 150:
+    elif BALL_RADIUS < 150 * SCALE:
         diff_text = font.render(f"NOOB", True, LIGHTBLUE)
     else:
         diff_text = font.render(f"BABY", True, WHITE)
-    diff_text_rect = diff_text.get_rect(center = (WIDTH / 2, HEIGHT / 2-200))
+    diff_text_rect = diff_text.get_rect(center = (WIDTH / 2, HEIGHT / 2-200 * SCALE))
     counter_text = font.render(f"BOUNCES: {counter}", True, WHITE)
-    counter_text_rect = counter_text.get_rect(center=(WIDTH / 2, HEIGHT / 2 + 200))
-    window.blit(diff_text, diff_text_rect)
-    window.blit(counter_text, counter_text_rect)
+    counter_text_rect = counter_text.get_rect(center=(WIDTH / 2, HEIGHT / 2 + 200 * SCALE))
+    high_res.blit(diff_text, diff_text_rect)
+    high_res.blit(counter_text, counter_text_rect)
+
+    scaled = pygame.transform.smoothscale(high_res, (ORIGINAL_WIDTH, ORIGINAL_HEIGHT))
+    window.blit(scaled, (0,0))
+
     pygame.display.flip()
     clock.tick(60)
 
